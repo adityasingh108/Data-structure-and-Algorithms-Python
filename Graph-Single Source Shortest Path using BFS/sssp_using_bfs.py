cb5f4912aa3788d8045015999ee6079ca637060e
adityasingh108 <adityasingh78085@gmail.com>
2022-01-11 01:54:53 +0530

    single shortest path problem usinf Bfs algorithms

---
+++ Graph-Single Source Shortest Path using BFS/sssp_using_bfs.py
@@ -0,0 +1,31 @@
+# solve single shortest paths problrm using bfs 
+from  collections import defaultdict
+class Graph:
+    def __init__(self, gdict=None):
+        if gdict is None:
+            gdict = {}
+        self.gdict = gdict
+        
+    def bfs(self, start, end):
+        queue = []
+        queue.append([start])
+        while queue:
+            path = queue.pop(0)
+            node = path[-1]
+            if node == end:
+                return path
+            for adjacent in self.gdict.get(node, []):
+                new_path = list(path)
+                new_path.append(adjacent)
+                queue.append(new_path)    
+                
+customDict = { "a" : ["b", "c"],
+               "b" : ["d", "g"],
+               "c" : ["d", "e"],
+               "d" : ["f"],
+               "e" : ["f"],
+               "g" : ["f"]
+            }             
+
+Graph = Graph(customDict)   
+print(Graph.bfs('a','f'))
